70e7d2acb16ac22c4b5cd88274b1934472d33062
Szymon Merski <merski70@gmail.com>
2022-10-21 19:57:27 +0200

    css wygral

--- frontend/src/MainGame/MainGame.js
+++ frontend/src/MainGame/MainGame.js
@@ -1,7 +1,6 @@
 import React from "react";
 import NavBar from "../NavBar/Navbar";
-import { Link } from "react-router-dom";
-
+import "./mainGame.css";
 function MainGame() {
   return (
     <div className="main-game-screen">
@@ -10,17 +9,19 @@ function MainGame() {
         <h1>Witaj w grze</h1>
         <p>
           W tej grze będziesz miał do wyboru 4 odpowiedzi. Wybierz jedną z nich
-          i zobacz, czy zgadłeś.
+          i ciesz się grą.
         </p>
-        <div className="main-game-screen-map">
-          <div className="main-game-screen-map__content">Mapa</div>
-        </div>
-        <div className="main-game-screen-quest">
-          <div className="main-game-screen-quest__content">Main Quest</div>
-        </div>
-        <div className="main-game-screen-equipment">
-          <div className="main-game-screen-equipment__content">Equipment</div>
-        </div>
+        <main>
+          <div className="main-game-screen-equipment">
+            <div className="main-game-screen-equipment__content">Equipment</div>
+          </div>
+          <div className="main-game-screen-map">
+            <div className="main-game-screen-map__content">Mapa</div>
+          </div>
+          <div className="main-game-screen-quest">
+            <div className="main-game-screen-quest__content">Main Quest</div>
+          </div>
+        </main>
       </div>
     </div>
   );
