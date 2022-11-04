4523ae9403ffa4f5e8dbe34809e668379a637b4f
Szymon Merski <merski70@gmail.com>
2022-11-04 11:42:10 +0100

    usuniecie smieci

--- frontend/src/PhoneView/UserGuide/UserGuide.js
+++ frontend/src/PhoneView/UserGuide/UserGuide.js
@@ -41,22 +41,3 @@ function UserGuide() {
 }
 
 export default UserGuide;
-
-/* <div id="nav-container">
-    <div class="bg"></div>
-    <div class="button" tabindex="0">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </div>
-    <div id="nav-content" tabindex="0">
-      <ul>
-        <li><a href="#0">Home</a></li>
-        <li><a href="#0">Services</a></li>
-        <li><a href="#0">Blog</a></li>
-        <li><a href="#0">About</a></li>
-        <li><a href="#0">Contact</a></li>
-        <li class="small"><a href="#0">Facebook</a><a href="#0">Instagram</a></li>
-      </ul>
-    </div>
-  </div> */
