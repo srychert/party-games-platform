236a473eaa571e01009a0430d0e9d7399516f74c
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-11-11 00:06:07 +0100

    code cleanup

--- backend/src/main/java/pl/srychert/PartyGamesPlatform/redis/RedisConfig.java
+++ backend/src/main/java/pl/srychert/PartyGamesPlatform/redis/RedisConfig.java
@@ -1,6 +1,5 @@
 package pl.srychert.PartyGamesPlatform.redis;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
