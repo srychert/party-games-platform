1883fb15d09e49c228f76c16a19ea73c74a12841
Sebastian Rychert <sebastian_rychert@wp.pl>
2023-03-28 11:38:06 +0200

    update player class

--- backend/src/main/java/pl/srychert/PartyGamesPlatform/model/game/Player.java
+++ backend/src/main/java/pl/srychert/PartyGamesPlatform/model/game/Player.java
@@ -2,9 +2,12 @@ package pl.srychert.PartyGamesPlatform.model.game;
 
 import lombok.Builder;
 import lombok.Data;
+import pl.srychert.PartyGamesPlatform.model.game.item.Item;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Data
 @Builder
@@ -18,10 +21,22 @@ public class Player {
     @Builder.Default
     private Integer atk = 2;
     @Builder.Default
+    private Integer speed = 2;
+    @Builder.Default
     private Integer hp = 20;
     @Builder.Default
     private List<Integer> availableOptions = new ArrayList<>();
     @Builder.Default
     private List<Integer> decisions = new ArrayList<>();
+    @Builder.Default
+    private Map<String, Item> items = new HashMap<>();
+
+    public void addItem(Item item) {
+        items.put(item.getId(), item);
+    }
+
+    public void removeItem(String itemId) {
+        items.remove(itemId);
+    }
 
 }
