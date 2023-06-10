c1110df05d672438305305354a2685dd03fcec68
Sebastian Rychert <sebastian_rychert@wp.pl>
2023-06-10 13:59:43 +0200

    add sort to games list

--- frontend/src/views/HostViews/List.jsx
+++ frontend/src/views/HostViews/List.jsx
@@ -5,6 +5,9 @@ import Loading from '../Loading';
 import { useState } from 'react';
 import GameCard from '../../components/GameCard/GameCard';
 import { useGames } from '../../hooks/game/useGames';
+import { IconContext } from 'react-icons';
+import { CgSearch, CgSelect } from 'react-icons/cg';
+import { useMemo } from 'react';
 
 function List() {
   let navigate = useNavigate();
@@ -17,26 +20,52 @@ function List() {
     return { ...g, type: 'game' };
   });
 
+  const allGames = [...games, ...quizzes];
+
   const handleChooseGame = (gameType, gameID) => {
     navigate(`/host/${gameType}/${gameID}`);
   };
 
-  const [searchPhrase, setSearchPhrase] = useState('');
+  const [query, setQuery] = useState('');
+  const [sortType, setSortType] = useState('ttp-desc');
 
   const handleSearchFilter = () => {
-    return [...games, ...quizzes].filter(
+    return allGames.filter(
       (game) =>
-        game.title.toLowerCase().includes(searchPhrase.toLowerCase()) ||
-        game.description.toLowerCase().includes(searchPhrase.toLowerCase())
+        game.title.toLowerCase().includes(query.toLowerCase()) ||
+        game.description.toLowerCase().includes(query.toLowerCase())
     );
   };
 
-  const silderFilter = () => {
-    const filteredGames = games.filter((game) => {
-      return game.totalTimesPlayed > 10;
-    });
-    return filteredGames;
+  const handleSort = (games) => {
+    switch (sortType) {
+      case 'ttp-desc':
+        games.sort((a, b) => (b.totalTimesPlayed > a.totalTimesPlayed ? 1 : -1));
+        break;
+      case 'ttp-asc':
+        games.sort((a, b) => (b.totalTimesPlayed > a.totalTimesPlayed ? -1 : 1));
+        break;
+      case 'title-desc':
+        games
+          .sort((a, b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()))
+          .reverse();
+        break;
+      case 'title-asc':
+        games.sort((a, b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()));
+      default:
+        break;
+    }
+    console.log(games);
+    return games;
   };
+
+  const filterAndSort = () => {
+    const filtered = handleSearchFilter();
+    const sorted = handleSort(filtered);
+    return sorted;
+  };
+
+  const visibleGames = useMemo(() => filterAndSort(), [allGames, query, sortType]);
 
   if (quizzesQuery.isLoading || gamesQuery.isLoading) {
     return <Loading />;
@@ -53,26 +82,53 @@ function List() {
 
   return (
     <div className="flex h-full w-full flex-col overflow-y-scroll">
-      {/* Filter */}
-      <div className="m-2 flex items-center justify-center">
-        <form className="flex flex-col">
-          <label htmlFor="gameName">Name</label>
-          <input
-            className="form-input"
-            type="text"
-            name="gameName"
-            id="gameName"
-            autoComplete="off"
-            onChange={(e) => setSearchPhrase(e.target.value)}
-          />
-        </form>
-      </div>
-      {/* Games */}
-      <div className="flex flex-wrap justify-center gap-5">
-        {handleSearchFilter().map((game) => (
-          <GameCard key={game.id} game={game} handleChooseGame={handleChooseGame} />
-        ))}
-      </div>
+      <IconContext.Provider
+        value={{
+          size: '1.25em',
+          style: {
+            position: 'absolute',
+            bottom: '8px',
+            right: '8px',
+            zIndex: 2,
+            color: 'black',
+          },
+        }}
+      >
+        {/* Filter */}
+        <div className="mb-8 flex flex-wrap items-center justify-center gap-4 border-b-2 p-6 sm:justify-between">
+          <div className="relative">
+            <input
+              className="form-input w-[200px]"
+              type="text"
+              name="query"
+              id="query"
+              autoComplete="off"
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Type to search"
+            />
+
+            <CgSearch />
+          </div>
+          <div className="relative">
+            <select
+              className="form-input w-[200px] pr-8"
+              onChange={(e) => setSortType(e.target.value)}
+            >
+              <option value="ttp-desc">Times Played - DESC</option>
+              <option value="ttp-asc">Times Played - ASC</option>
+              <option value="title-asc">Title - ASC</option>
+              <option value="title-desc">Title - DESC</option>
+            </select>
+            <CgSelect />
+          </div>
+        </div>
+        {/* Games */}
+        <div className="flex flex-wrap justify-center gap-6 px-6">
+          {visibleGames.map((game) => (
+            <GameCard key={game.id} game={game} handleChooseGame={handleChooseGame} />
+          ))}
+        </div>
+      </IconContext.Provider>
     </div>
   );
 }
