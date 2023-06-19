407a96af7e72d0caceaf926b95804edee8d9983d
Szymon Merski <merski70@gmail.com>
2023-06-19 10:44:40 +0200

    colors for knights

--- frontend/src/components/HostView/MainGamePodium.jsx
+++ frontend/src/components/HostView/MainGamePodium.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Stats from '../PhoneView/Game/Stats';
 import hostContext from '../../context/HostContext';
 
@@ -10,16 +10,32 @@ function MainGamePodium() {
       .sort((a, b) => b.gold - a.gold)
   );
 
-  console.log(playersArray[0]);
+  useEffect(() => {
+    setPlayersArray(
+      Object.keys(players)
+        .map((key, index) => players[key])
+        .sort((a, b) => b.gold - a.gold)
+    );
+  }, [players]);
 
   // if (playersArray.length < 3) return null;
   return (
     <>
       <div className="mx-5 flex flex-wrap items-center justify-center">
         <div id="podium" className="grid grid-cols-3 gap-4">
-          {playersArray.map((player) => (
-            <Stats key={player.id} entity={player} />
-          ))}
+          {playersArray.length > 0 && (
+            <Stats entity={{ ...playersArray[0], path: 'knight1.png' }} />
+          )}
+          {playersArray.length > 1 && (
+            <Stats entity={{ ...playersArray[1], path: 'knight2.png' }} />
+          )}
+          {playersArray.length > 2 && (
+            <Stats entity={{ ...playersArray[2], path: 'knight3.png' }} />
+          )}
+          {playersArray.length > 3 &&
+            playersArray
+              .slice(0, 3)
+              .map((player) => <Stats key={player.id} entity={player} />)}
         </div>
       </div>
     </>
